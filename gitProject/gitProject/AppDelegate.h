13433ccfbe49a5fb9f76edd2e5c72e1c1ffda5b5
吕洪建 <1353462015@qq.com>
2018-10-17 16:38:27 +0800

    修改了什么内容

---
+++ gitProject/gitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitProject
+//
+//  Created by 祥云创想 on 2018/10/17.
+//  Copyright © 2018年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitProject/gitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitProject
+//
+//  Created by 祥云创想 on 2018/10/17.
+//  Copyright © 2018年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
